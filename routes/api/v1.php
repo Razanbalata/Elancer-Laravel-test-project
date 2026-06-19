9c782a41676db5ab34551f0e40967166a311c2a0
Razanbalata <razanbalata@gmail.com>
2026-06-19 16:07:48 +0300

    add delete access token for the current user

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/access-tokens',[AccessTokenController::class,'store'])
 ->middleware('guest:sanctum'); 
+
+Route::post('auth/access-tokens/{token?}',[AccessTokenController::class,'destroy'])
+->middleware('guest:sanctum'); 
+
+
 Route::apiResource('posts',PostController::class)
 ->middlewareFor(['store','update','destroy'],['auth:sanctum']);
 
